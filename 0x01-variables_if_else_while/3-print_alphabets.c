ca8d8920fc62dc66a222a1e7acef9081f34df67e
ben-Ade <benardvictor02@gmail.com>
2022-10-14 19:08:40 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char low;
+char high;
+for (low = 'a'; low <= 'z'; low++)
+putchar(low);
+for (high = 'A'; high <= 'Z'; high++)
+putchar(high);
+printf("\n");
+return (0);
+}
