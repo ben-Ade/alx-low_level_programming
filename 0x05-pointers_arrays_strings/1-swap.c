202813441ba0d161786769d2c2adef22574127d3
ben-Ade <benardvictor02@gmail.com>
2022-10-24 10:27:05 -0700

    Task 2

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,13 @@
 #include "main.h"
 /**
-*MAin--Swap Pointers
-*
-* Return: void
+*swap_int - swap values of two integers
+*@a: pointer to an integer
+*@b: pointer to an integer
 */
 void swap_int(int *a, int *b)
 {
-strcpy(*a, *b);
+int c;
+c = *a;
+*a = *b;
+*b = c;
 }
