6f6209685ede30f5ea3974771f71de9c23c3e22b
ben-Ade <benardvictor02@gmail.com>
2022-10-31 09:45:57 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,12 +2,13 @@
 /**
 * _memset - function fills the first n bytes of mem
 * @s : pointer to memory area
-* @i: array count
 * @b: constant byte
 * @n: number of bytes to fill
 * Return: a pointer to memory area @s
 */
-char *_memset(char *s, char b, unsigned int n, unsigned int i)
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
 for (i = 0; i < n; i++)
 {
 s[i] = b;
